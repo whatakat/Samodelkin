ea92cfa68205efb38f0622f487f49fc3fd64d8b2
Leoneed <40763564+whatakat@users.noreply.github.com>
2020-11-29 15:47:51 +0300

    add extension in Bundle

--- app/src/main/java/com/bankmtk/samodelkin/MainActivity.kt
+++ app/src/main/java/com/bankmtk/samodelkin/MainActivity.kt
@@ -8,22 +8,24 @@ import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
 
+private var Bundle.characterData
+get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
 class MainActivity : AppCompatActivity() {
     private var characterData = CharacterGenerator.generate()
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        outState.characterData = characterData
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        characterData = savedInstanceState?.let {
-            it.getSerializable(CHARACTER_DATA_KEY)
-            as CharacterGenerator.CharacterData
-        }?:CharacterGenerator.generate()
+        characterData = savedInstanceState?.characterData ?:
+            CharacterGenerator.generate()
 
         generateButton.setOnClickListener {
             characterData = CharacterGenerator.generate()
